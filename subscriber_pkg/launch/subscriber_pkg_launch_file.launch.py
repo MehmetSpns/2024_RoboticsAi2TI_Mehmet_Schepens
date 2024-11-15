f2c8c2bc6748dc8d891f053910e81282b84993cd
DeTeletubyLord <mehmet.schepens@student.ehb.be>
2024-11-15 12:06:21 +0100

    Subscribing to topics (lidar data)

---
+++ subscriber_pkg/launch/subscriber_pkg_launch_file.launch.py
@@ -0,0 +1,13 @@
+
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='subscriber_pkg',
+            executable='simple_subscriber',
+            output='screen'),
+    ])
+
